e751d3db3b2cfffbd0706c5a83368e65dddead8d
Rajarsi Saha <rajarsi3997@gmail.com>
2019-10-28 22:40:30 +0530

    Add files via upload

--- tsp.c
+++ tsp.c
@@ -1,88 +1,73 @@
-#include<stdio.h>
+// C++ implementation of the approach
+#include <bits/stdc++.h>
+using namespace std;
+#define V 4
 
-int least(int c);
- 
-int ary[10][10],completed[10],n,cost=0;
- 
-void takeInput()
+// Function to find the minimum weight Hamiltonian Cycle
+void tsp(int graph[][V], vector<bool>& v, int currPos,
+		int n, int count, int cost, int& ans)
 {
-	int i,j;
- 
-	printf("Enter the number of villages: ");
-	scanf("%d",&n);
- 
-	printf("\nEnter the Cost Matrix\n");
- 
-	for(i=0;i < n;i++)
-		for( j=0;j < n;j++)
-		{
-			scanf("%d",&ary[i][j]);
- 
-		completed[i]=0;
-	}
- 
-	printf("\n\nThe cost list is:");
- 
-	for( i=0;i < n;i++)
-	{
-		printf("\n");
- 
-		for(j=0;j < n;j++)
-			printf("\t%d",ary[i][j]);
-	}
-}
- 
-void mincost(int city)
-{
-	int i,ncity;
- 
-	completed[city]=1;
- 
-	printf("%d--->",city+1);
-	ncity=least(city);
- 
-	if(ncity==999)
-	{
-		ncity=0;
-		printf("%d",ncity+1);
-		cost+=ary[city][ncity];
- 
+
+	// If last node is reached and it has a link
+	// to the starting node i.e the source then
+	// keep the minimum value out of the total cost
+	// of traversal and "ans"
+	// Finally return to check for more possible values
+	if (count == n && graph[currPos][0]) {
+		ans = min(ans, cost + graph[currPos][0]);
+		cout<<"\n cost is :"<<ans<<"for visiting the city. ";
 		return;
 	}
- 
-	mincost(ncity);
-}
- 
-int least(int c)
-{
-	int i,nc=999;
-	int min=999,kmin;
- 
-	for(i=0;i < n;i++)
-	{
-		if((ary[c][i]!=0)&&(completed[i]==0))
-			if(ary[c][i]+ary[i][c] < min)
-			{
-				min=ary[i][0]+ary[c][i];
-				kmin=ary[c][i];
-				nc=i;
-			}
+
+	// BACKTRACKING STEP
+	// Loop to traverse the adjacency list
+	// of currPos node and increasing the count
+	// by 1 and cost by graph[currPos][i] value
+	for (int i = 0; i < n; i++) {
+		if (!v[i] && graph[currPos][i]) {
+
+			// Mark as visited
+			v[i] = true;
+			cout<<"\ncity visited: "<<currPos<<" to :"<<i<< "cost: "<<cost + graph[currPos][i];
+			tsp(graph, v, i, n, count + 1,
+				cost + graph[currPos][i], ans);
+
+			// Mark ith node as unvisited
+			v[i] = false;
+		}
 	}
- 
-	if(min!=999)
-		cost+=kmin;
- 
-	return nc;
-}
- 
+};
+
+// Driver code
 int main()
 {
-	takeInput();
- 
-	printf("\n\nThe Path is:\n");
-	mincost(0);
- 
-	printf("\n\nMinimum cost is %d\n ",cost);
- 
+	// n is the number of nodes i.e. V
+	int n = 4;
+
+	int graph[][V] = {
+		{ 0, 10, 15, 20 },
+		{ 10, 0, 35, 25 },
+		{ 15, 35, 0, 30 },
+		{ 20, 25, 30, 0 }
+	};
+
+	// Boolean array to check if a node
+	// has been visited or not
+	vector<bool> v(n);
+	for (int i = 0; i < n; i++)
+		v[i] = false;
+
+	// Mark 0th node as visited
+	v[0] = true;
+	int ans = INT_MAX;
+
+	// Find the minimum weight Hamiltonian Cycle
+	//tsp(int graph[][V], vector<bool>& v, int currPos,
+	//	int n, int count, int cost, int& ans)
+	tsp(graph, v, 0, n, 1, 0, ans);
+
+	// ans is the minimum weight Hamiltonian Cycle
+	cout << ans;
+
 	return 0;
 }
