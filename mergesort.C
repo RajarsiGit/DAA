d403eb172a18dd8b71cb1e6b060eed702ae7bebd
Rajarsi Saha <rajarsi3997@gmail.com>
2019-11-05 10:32:49 +0530

    Update mergesort.C

--- mergesort.C
+++ mergesort.C
@@ -44,23 +44,29 @@ void mergeSort(int arr[], int l, int r)
 
 void printArray(int A[], int size)
 {
-    int i;
-    for (i=0; i < size; i++)
+    for (int i=0; i < size; i++)
         printf("%d ", A[i]);
     printf("\n");
 }
 
 int main()
 {
-    int arr[] = {12, 11, 13, 5, 6, 7};
-    int arr_size = sizeof(arr)/sizeof(arr[0]);
+    int *arr, arr_size, i;
+    
+    printf("Enter array size: ");
+    scanf("%d",&arr_size);
+    arr = (int*)malloc(arr_size*sizeof(int));
+    
+    printf("\nEnter elements: ");
+    for(i=0;i<arr_size;i++)
+        scanf("%d",&arr[i]);
 
-    printf("Given array is \n");
+    printf("Unsorted array: \n");
     printArray(arr, arr_size);
 
     mergeSort(arr, 0, arr_size - 1);
 
-    printf("\nSorted array is \n");
+    printf("\nSorted array: \n");
     printArray(arr, arr_size);
     return 0;
 }
